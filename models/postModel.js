852c3c321a3c2487755b759f79732e20ad296466
RyBhrdwj <25ricky08@gmail.com>
2024-08-16 16:06:46 +0530

    update models

--- models/postModel.js
+++ models/postModel.js
@@ -6,6 +6,10 @@ const postSchema = new mongoose.Schema({
     ref: 'User',
     required: true,
   },
+  imageUrl: {
+    type: String,
+    required: true
+  },
   createdAt: {
     type: Date,
     default: Date.now,
